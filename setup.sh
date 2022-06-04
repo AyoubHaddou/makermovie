8bfaf0065cb5efbea76455ae4c6fbebefe1cca13
ayoub <ayoubhaddou1@gmail.com>
2022-06-04 23:43:01 +0200

    try to add theme at setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 cd ~/.streamlit/
 
-echo "\
+echo "\[theme]
+primaryColor='#584BFF'
+backgroundColor='#0E1117'
+secondaryBackgroundColor='#262730'
+textColor='#FAFAFA'
+font='sans serif'\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
